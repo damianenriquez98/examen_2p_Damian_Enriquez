1c5c2c6658a89fda4a74774cd1ac93195754fe26
damianenriquez98 <damianvalle19@gmail.com>
2021-08-18 19:54:03 -0500

    19:54

---
+++ app/src/main/java/com/example/proyectofinal/Remote/IMyAPICategoria.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinal.Remote;
+
+import com.example.proyectofinal.Model.tblCategoria;
+
+
+import io.reactivex.Observable;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+
+
+public interface IMyAPICategoria {
+    @GET("api/User/login")
+    Observable<String>mostrarCategorias(@Body tblCategoria categoria);
+}
