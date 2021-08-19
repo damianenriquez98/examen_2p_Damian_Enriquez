88a51d0a533030c53fb01664b42c47c992486f88
damianenriquez98 <damianvalle19@gmail.com>
2021-08-18 20:18:23 -0500

    Guardado

--- app/src/main/java/enriquez/damian/proyectofinal/UserService.java
+++ app/src/main/java/enriquez/damian/proyectofinal/UserService.java
@@ -5,10 +5,6 @@ import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface UserService {
-
-
     @POST("login/")
     Call<LoginResponse> userLogin(@Body LoginRequest loginRequest);
-
-
 }
